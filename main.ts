e2e2e132b62b278b48d0b31e81cc1c221f00edf0
Tecnoprofedavid <103004579+Tecnoprofedavid@users.noreply.github.com>
2022-04-11 20:17:10 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("A A G F F E D D ", 120)
+    basic.showString("" + (input.compassHeading()))
+})
+basic.forever(function () {
+	
+})
